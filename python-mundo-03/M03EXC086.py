c6b51c09ba8b62d20a1f13958b47c9bad7cc939a
Luan Eike <luaneike.contato@gmail.com>
2023-07-07 19:21:22 -0300

    Update M03EXC086.py

--- python-mundo-03/M03EXC086.py
+++ python-mundo-03/M03EXC086.py
@@ -2,6 +2,5 @@ matriz = [[],[],[]]
 for indice in range(0,3):
     for i in range(0,3):
         matriz[indice].append(int(input(f'Digite um valor para [{indice}, {i}]: ')))
-for indice in range(0,3):
-    for i in range(0,3):
-        print(matriz[indice][i])
+for i in range(0,3):
+    print(f'[{matriz[i][0]:^5}]',f'[{matriz[i][1]:^5}]',f'[{matriz[i][2]:^5}]')

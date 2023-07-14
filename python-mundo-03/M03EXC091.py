2a63cb1939e3c57a0ec8465610b701d64851674f
Luan Eike <luaneike.contato@gmail.com>
2023-07-13 21:12:52 -0300

    Update M03EXC091.py

--- python-mundo-03/M03EXC091.py
+++ python-mundo-03/M03EXC091.py
@@ -6,21 +6,12 @@ jogadores = {}
 for i in range(1,5):
     jogadores[f'Jogador{i}'] = rd(1,6)
 
-for key, v in jogadores.items():
-    print(f'O {key} tirou {v} no dado')
-    # sleep(1)
+for k, v in jogadores.items():
+    print(f'O {k} tirou {v} no dado')
+    sleep(1)
 print('-='*30)
 print('== RANKING DOS JOGADORES ==')
 
-# for i in range(1,5):
-#     print(sorted(jogadores.values(),reverse=True))
-#     print(f'{i}° lugar: {key} com {v}')
-
-for i, v in enumerate(sorted(jogadores.values(), reverse=True)):
-    for i in jogadores.values():
-        if jogadores.keys() == v:
-            j = jogadores.keys()
-            print(f'{i+1} lugar: {j} com {v}')
-
-for ind, item in enumerate(jogadores.values()):
-    print(f'{ind} and {item}')
+for ind, i in enumerate(sorted(jogadores, key = jogadores.get, reverse=True)):
+    print(f'{ind+1}° lugar: {i} com {jogadores[i]}')
+    sleep(1)
